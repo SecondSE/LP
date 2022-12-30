d3f150f97034f6b2afea300fbed15476312c43fe
hermandhillon <dhillonherman1991@gmail.com>
2022-12-29 17:05:46 -0800

    correct index.tsx , merge other alexs components

--- pages/components/main/Main.tsx
+++ pages/components/main/Main.tsx
@@ -1,6 +1,9 @@
 import { StyledMain, StyledImage } from "../styles/Main.styled";
 import Hero from "../main/Hero";
 import Action from "../main/Action";
+import Contact from "../contact/Contact";
+import ClientHistory from "../clientHistory/ClientHistory";
+import AboutUs from "../aboutUs/AboutUs";
 import sanHolo from "../../../assets/san-holo.png";
 
 export default function Main() {
@@ -8,6 +11,9 @@ export default function Main() {
     <StyledMain>
       <Hero />
       <Action />
+      <AboutUs />
+      <ClientHistory />
+      <Contact />
     </StyledMain>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
-import App from "./components/App";
+import GlobalStyle from "./components/styles/Global.styled";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
 
 export default function Index() {
-  return <App />;
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
