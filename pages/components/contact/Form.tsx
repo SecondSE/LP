aec895a342e739c3d605e195f7ac07f4acaeab5c
Alex Salazar <helloworld3197@gmail.com>
2023-01-13 18:08:50 -0500

    Added formspree/react and imported into form

--- pages/components/contact/Form.tsx
+++ pages/components/contact/Form.tsx
@@ -1,35 +1,82 @@
-import {StyledInputName, StyledForm, StyledTextArea, StyledLabel, StyledInput, StyledInputSubmitSection, StyledInputNameSection, StyledInputSubmit, StyledInputAndLabelSection} from "../styles/Contact.styled"
+import {
+  StyledInputName,
+  StyledForm,
+  StyledTextArea,
+  StyledLabel,
+  StyledInput,
+  StyledInputSubmitSection,
+  StyledInputNameSection,
+  StyledInputSubmit,
+  StyledInputAndLabelSection,
+} from "../styles/Contact.styled";
+import { useForm, ValidationError } from "@formspree/react";
 
-export default function Form(){
-    return(
+export default function Form() {
+  const [state, handleSubmit] = useForm("xlekdrav");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
+
+  return (
     <>
-        <StyledForm action="" method="get" id="contact-form">
-            <StyledInputAndLabelSection>
-                <StyledLabel>Name*</StyledLabel>
-                <StyledInputNameSection>
-                    <StyledInputName type="text" placeholder=" First Name" required/>
-                    <StyledInput type="text" placeholder=" Last Name"  required/>
-                </StyledInputNameSection>
-            </StyledInputAndLabelSection>
-            
-            <StyledInputAndLabelSection>
-                <StyledLabel>Phone Number*</StyledLabel>
-                <StyledInput type="tel" placeholder=" 555 555-555" required/>
-            </StyledInputAndLabelSection>
-            <StyledInputAndLabelSection>
-                <StyledLabel>Email Address*</StyledLabel>
-                <StyledInput type="email" placeholder=" example@email.com" required/>
-            </StyledInputAndLabelSection>
-            <StyledInputAndLabelSection>
-                <StyledLabel>Question/Comments*</StyledLabel>
-                <StyledTextArea form="contact-form" placeholder=" Tell us what you're thinking" required/>
-            </StyledInputAndLabelSection>
-            <StyledInputSubmitSection>
-                <StyledInputSubmit type="submit" value="SUBMIT"/>
-            </StyledInputSubmitSection>
-            
-        </StyledForm>
+      <StyledForm onSubmit={handleSubmit} method="POST" id="contact-form">
+        <StyledInputAndLabelSection>
+          <StyledLabel>Name*</StyledLabel>
+          <StyledInputNameSection>
+            <StyledInputName
+              id="name"
+              name="name"
+              type="text"
+              placeholder=" First Name"
+              required
+            />
+            <StyledInput
+              id="name"
+              name="name"
+              type="text"
+              placeholder=" Last Name"
+              required
+            />
+          </StyledInputNameSection>
+        </StyledInputAndLabelSection>
 
+        <StyledInputAndLabelSection>
+          <StyledLabel>Phone Number*</StyledLabel>
+          <StyledInput type="tel" placeholder=" 555 555-555" required />
+        </StyledInputAndLabelSection>
+        <StyledInputAndLabelSection>
+          <StyledLabel>Email Address*</StyledLabel>
+          <StyledInput
+            id="email"
+            name="email"
+            type="email"
+            placeholder=" example@email.com"
+            required
+          />
+        </StyledInputAndLabelSection>
+        <StyledInputAndLabelSection>
+          <StyledLabel>Question/Comments*</StyledLabel>
+          <StyledTextArea
+            id="message"
+            name="message"
+            // form="contact-form"
+            placeholder=" Tell us what you're thinking"
+            required
+          />
+        </StyledInputAndLabelSection>
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <StyledInputSubmitSection>
+          <StyledInputSubmit
+            type="submit"
+            value="SUBMIT"
+            disabled={state.submitting}
+          />
+        </StyledInputSubmitSection>
+      </StyledForm>
     </>
-    )
+  );
 }
