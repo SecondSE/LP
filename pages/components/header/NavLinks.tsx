259ab17daea2186121c9e98d38002dd28aee4ea7
Alex Salazar <helloworld3197@gmail.com>
2023-01-18 19:37:38 -0500

    Added additional updates to header

--- pages/components/header/NavLinks.tsx
+++ pages/components/header/NavLinks.tsx
@@ -3,7 +3,6 @@ import {
   MobileNavListItem,
   NavListLink,
 } from "../styles/Header.styled";
-import { MenuWrapper } from "../styles/Wrappers.styled";
 import { Link } from "react-scroll";
 
 interface NavLinkProps {
@@ -16,38 +15,36 @@ const NavLinks: React.FC<NavLinkProps> = function ({
   closeMobileMenu,
 }) {
   return (
-    <MenuWrapper>
-      <MobileNavList>
-        <MobileNavListItem onClick={() => isMobile && closeMobileMenu()}>
-          SERVICES
-          <NavListLink
-            href="#services"
-            aria-label="navigation link to services section"
-          />
-        </MobileNavListItem>
-        <MobileNavListItem onClick={() => isMobile && closeMobileMenu()}>
-          ABOUT
-          <NavListLink
-            href="#about"
-            aria-label="navigation link to about section"
-          />
-        </MobileNavListItem>
-        <MobileNavListItem onClick={() => isMobile && closeMobileMenu()}>
-          WORK
-          <NavListLink
-            href="#work"
-            aria-label="navigation link to work section"
-          />
-        </MobileNavListItem>
-        <MobileNavListItem onClick={() => isMobile && closeMobileMenu()}>
-          CONTACT
-          <NavListLink
-            href="#contact"
-            aria-label="navigation link to contact section"
-          />
-        </MobileNavListItem>
-      </MobileNavList>
-    </MenuWrapper>
+    <MobileNavList>
+      <MobileNavListItem onClick={() => isMobile && closeMobileMenu()}>
+        SERVICES
+        <NavListLink
+          href="#services"
+          aria-label="navigation link to services section"
+        />
+      </MobileNavListItem>
+      <MobileNavListItem onClick={() => isMobile && closeMobileMenu()}>
+        ABOUT
+        <NavListLink
+          href="#about"
+          aria-label="navigation link to about section"
+        />
+      </MobileNavListItem>
+      <MobileNavListItem onClick={() => isMobile && closeMobileMenu()}>
+        WORK
+        <NavListLink
+          href="#work"
+          aria-label="navigation link to work section"
+        />
+      </MobileNavListItem>
+      <MobileNavListItem onClick={() => isMobile && closeMobileMenu()}>
+        CONTACT
+        <NavListLink
+          href="#contact"
+          aria-label="navigation link to contact section"
+        />
+      </MobileNavListItem>
+    </MobileNavList>
   );
 };
 export default NavLinks;
